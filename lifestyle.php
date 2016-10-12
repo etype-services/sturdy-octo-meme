70b22e30d5817e6c043b937063c72c59f60a6511
etypeweb@delphos.c.modern-photon-112115.internal <etypeweb@delphos.c.modern-photon-112115.internal>
2016-10-12 19:54:58 +0000

    updates

--- lifestyle.php
+++
@@ -1,7 +0,0 @@
-<?php
-$url = $_SERVER['HTTP_HOST'];
-if($url == 'eastlandcountytoday.com' || $url == 'www.eastlandcountytoday.com')
-{
- require_once('sites/eastlandcountytoday.com/rssfeed/lifestyle.inc');
-}
-?>
