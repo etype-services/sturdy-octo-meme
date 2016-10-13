1e5eb695ab4e197d81326c199a170d44b98d71a0
Charlie Llewellin <charlie@llewellin.net>
2016-10-13 15:23:26 -0500

    southreporter

--- sites/sites.php
+++ sites/sites.php
@@ -4,3 +4,9 @@ $sites['chamberlainsun.etypegoogle9.com'] = 'chamberlainsun.etypegoogle9.com';
 $sites['chamberlainsun-drupal.etypegoogle9.com'] = 'chamberlainsun.etypegoogle9.com';
 $sites['chamberlainsun.com'] = 'chamberlainsun.etypegoogle9.com';
 $sites['www.chamberlainsun.com'] = 'chamberlainsun.etypegoogle9.com';
+
+$sites['southreporter.etypegoogle9.com'] = 'southreporter.etypegoogle9.com';
+$sites['southreporter-drupal.etypegoogle9.com'] = 'southreporter.etypegoogle9.com';
+$sites['southreporter.com'] = 'southreporter.etypegoogle9.com';
+$sites['www.southreporter.com'] = 'southreporter.etypegoogle9.com';
+
