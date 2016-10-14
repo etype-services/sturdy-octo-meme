c4448f5d1bda327d96ae7255e5ed26fab9da2ed2
etypeweb@delphos.c.modern-photon-112115.internal <etypeweb@delphos.c.modern-photon-112115.internal>
2016-10-14 18:53:24 +0000

    gatesville messenger

--- sites/sites.php
+++ sites/sites.php
@@ -1,7 +1,6 @@
 <?php
 
 $sites['chamberlainsun.etypegoogle9.com'] = 'chamberlainsun.etypegoogle9.com';
-$sites['chamberlainsun-drupal.etypegoogle9.com'] = 'chamberlainsun.etypegoogle9.com';
 $sites['chamberlainsun.com'] = 'chamberlainsun.etypegoogle9.com';
 $sites['www.chamberlainsun.com'] = 'chamberlainsun.etypegoogle9.com';
 
