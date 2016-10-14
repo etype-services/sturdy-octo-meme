a23fee3da9630439f4f083c875f9bd3b5656b5d1
Charlie Llewellin <charlie@llewellin.net>
2016-10-14 14:12:06 -0500

    gatesvillemessenger

--- sites/sites.php
+++ sites/sites.php
@@ -8,9 +8,3 @@ $sites['southreporter.etypegoogle9.com'] = 'southreporter.etypegoogle9.com';
 $sites['southreporter-drupal.etypegoogle9.com'] = 'southreporter.etypegoogle9.com';
 $sites['southreporter.com'] = 'southreporter.etypegoogle9.com';
 $sites['www.southreporter.com'] = 'southreporter.etypegoogle9.com';
-
-$sites['gatesvillemessenger.etypegoogle9.com'] = 'gatesvillemessenger.etypegoogle9.com';
-$sites['gatesvillemessenger-drupal.etypegoogle9.com'] = 'gatesvillemessenger.etypegoogle9.com';
-$sites['gatesvillemessenger.com'] = 'gatesvillemessenger.etypegoogle9.com';
-$sites['www.gatesvillemessenger.com'] = 'gatesvillemessenger.etypegoogle9.com';
-
