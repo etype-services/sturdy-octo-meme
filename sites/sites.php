1bd0d528e4af35129ee7c97c77dc1b9bdd688d8c
Charlie Llewellin <charlie@llewellin.net>
2016-10-16 17:29:10 -0500

    adding sites

--- sites/sites.php
+++ sites/sites.php
@@ -5,6 +5,25 @@ $sites['chamberlainsun.com'] = 'chamberlainsun.etypegoogle9.com';
 $sites['www.chamberlainsun.com'] = 'chamberlainsun.etypegoogle9.com';
 
 $sites['southreporter.etypegoogle9.com'] = 'southreporter.etypegoogle9.com';
-$sites['southreporter-drupal.etypegoogle9.com'] = 'southreporter.etypegoogle9.com';
 $sites['southreporter.com'] = 'southreporter.etypegoogle9.com';
 $sites['www.southreporter.com'] = 'southreporter.etypegoogle9.com';
+
+$sites['ainsworthnews.etypegoogle9.com'] = 'ainsworthnews.etypegoogle9.com';
+$sites['ainsworthnews-drupal.etypegoogle9.com'] = 'ainsworthnews.etypegoogle9.com';
+$sites['ainsworthnews.com'] = 'ainsworthnews.etypegoogle9.com';
+$sites['www.ainsworthnews.com'] = 'ainsworthnews.etypegoogle9.com';
+
+$sites['deweycountyrecord.etypegoogle9.com'] = 'deweycountyrecord.etypegoogle9.com';
+$sites['deweycountyrecord-drupal.etypegoogle9.com'] = 'deweycountyrecord.etypegoogle9.com';
+$sites['deweycountyrecord.com'] = 'deweycountyrecord.etypegoogle9.com';
+$sites['www.deweycountyrecord.com'] = 'deweycountyrecord.etypegoogle9.com';
+
+$sites['okeenerecord.etypegoogle9.com'] = 'okeenerecord.etypegoogle9.com';
+$sites['okeenerecord-drupal.etypegoogle9.com'] = 'okeenerecord.etypegoogle9.com';
+$sites['okeenerecord.com'] = 'okeenerecord.etypegoogle9.com';
+$sites['www.okeenerecord.com'] = 'okeenerecord.etypegoogle9.com';
+
+$sites['columbusnews-report.etypegoogle9.com'] = 'columbusnews-report.etypegoogle9.com';
+$sites['columbusnews-report-drupal.etypegoogle9.com'] = 'columbusnews-report.etypegoogle9.com';
+$sites['columbusnews-report.com'] = 'columbusnews-report.etypegoogle9.com';
+$sites['www.columbusnews-report.com'] = 'columbusnews-report.etypegoogle9.com';
