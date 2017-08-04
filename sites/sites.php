b0cfacaa588090d125d73f91aeba838d6a7d8023
Charlie Llewellin <charlie@llewellin.net>
2017-08-04 10:45:12 -0500

    freemansd

--- sites/sites.php
+++ sites/sites.php
@@ -161,3 +161,7 @@ $sites['beckhamcountyrecord.com'] = 'beckhamcountyrecord.etypegoogle9.com';
 $sites['www.beckhamcountyrecord.com'] = 'beckhamcountyrecord.etypegoogle9.com';
 $sites['sayrerecord.com'] = 'beckhamcountyrecord.etypegoogle9.com';
 $sites['www.sayrerecord.com'] = 'beckhamcountyrecord.etypegoogle9.com';
+
+$sites['freemansd.etypegoogle9.com'] = 'freemansd.etypegoogle9.com';
+$sites['freemansd.com'] = 'freemansd.etypegoogle9.com';
+$sites['www.freemansd.com'] = 'freemansd.etypegoogle9.com';
